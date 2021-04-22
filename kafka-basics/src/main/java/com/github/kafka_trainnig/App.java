3d8cfd2f3c86493600f70063bf1a2752c086cb65
Bar Snir <bars@Admins-MacBook-Pro.local>
2021-04-22 12:44:49 +0300

    changed repo dir structure

---
+++ kafka-basics/src/main/java/com/github/kafka_trainnig/App.java
@@ -0,0 +1,13 @@
+package com.github.kafka_trainnig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
